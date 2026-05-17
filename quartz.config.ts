636b51fbb954d16e8a6791e0c4a0e0c5261b10f2
samposalmela <samposalmela111@gmail.com>
2026-05-17 15:47:52 +0300

    add notes

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "youaremyrival.github.io/druptur-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
